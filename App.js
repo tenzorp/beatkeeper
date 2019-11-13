20ffe9eb20118f68a3a5cd1b43196564da334943
Zach Levitt <zlevitt@middlebury.edu>
2019-11-13 00:30:59 -0500

    swiping

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { PropTypes, Component } from 'react';
+import { createAppContainer } from 'react-navigation';
+import { Text, View, StyleSheet,Button } from 'react-native';
+import Swiper from 'react-native-swiper'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+console.disableYellowBox = true;
+
+export default class App extends Component {
+  render() {
+    return (
+   	<Swiper
+        loop={false}
+        showsPagination={false}
+        index={1}>
+        <View style={styles.container}>
+          <Text style={styles.player}>SINGLE PLAYER</Text>
+        </View>
+    <Swiper 
+    	showsPagination={false} 
+    	loop={false}
+    	horizontal={false}>
+        <View style={styles.container}>
+      		<Text style={styles.header}>BEAT KEEPER</Text>
+    	</View>
+    	<View style={styles.container}>
+         	<Text style={styles.player}>PROFILE</Text>
+        </View>
+        </Swiper>
+
+        <View style={styles.container}>
+          <Text style={styles.player}>MULTIPLAYER</Text>
+        </View>
+
+      </Swiper>);
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize:80,
+    textAlign: 'center',
+  },
+  player: {
+  	fontSize: 30,
+  	textAlign: 'center'
+  }
 });
