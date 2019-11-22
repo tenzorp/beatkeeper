bf1b625f3c72b0bb052e05bc66df4320a2a57df4
Hamza Kiyani <mkiyani@middlebury.edu>
2019-11-21 19:15:04 -0500

    stat/profile has input

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import React from 'react';
+
+// make state for text
+const [text, setText] = useState("");
 
 export default function ProfileScreen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Profile</Text>
+            <Text style={styles.text}>Name</Text>
+            <Text style={styles.text}>Statistics</Text>
+            <TextInput
+                style={{height: 40}}
+                placeholder="Enter your Name!"
+                onChangeText={(text) => setText({text})}
+                value={text}
+            />
+            <Text style={styles.text}>You have played 1 game!</Text>
         </View>
     );
 }
