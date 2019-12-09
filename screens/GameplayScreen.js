8ce4f81edede4849153e1067bd8a14d9ce36ca08
Tenzin Dorjee <tdorjee@middlebury.edu>
2019-12-08 23:05:39 -0500

    added modal for settings

--- screens/GameplayScreen.js
+++ screens/GameplayScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useFirestore, useFirestoreConnect } from 'react-redux-firebase';
@@ -43,11 +43,33 @@ export default function GameplayScreen(props) {
     props.navigation.navigate('Feedback', { level: props.navigation.getParam('level') });
   }, 5000);
 
+
   return (
     <View style={styles.container}>
+      <Modal isVisible={modal}>
+        <View style={styles.modal}>
+          <Text style={styles.modalTitle}>PAUSED</Text>
+          <TouchableOpacity>
+            <Text style={styles.text} onPress={() => setModal(!modal)}>Resume</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.text}>Retry</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.text}>Settings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.text} onPress={() => props.navigation.navigate('Home')}>Home</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
       <View style={styles.headerView}>
-        <AntDesign name={'arrowleft'} size={50} color={'#FFFFFF'}  />
-        <Foundation name={'pause'} size={50} color={'#FFFFFF'}  />
+        <TouchableOpacity>
+          <AntDesign name={'arrowleft'} size={50} color={'#FFFFFF'} onPress={()=> props.navigation.navigate('Levels')} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Foundation name={'pause'} size={50} color={'#FFFFFF'}  onPress={()=> setModal(!modal)}/>
+        </TouchableOpacity>
       </View>
       <View style={styles.titleView}>
         <Text style={styles.text}>Tap on the screen when the hexagons match!</Text>
@@ -88,11 +110,30 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: '2%',
-    marginBottom: '2%',
   },
   hexagons: {
     flex: 8,
     width: '100%',
   },
+  modal: {
+    alignItems: 'center',
+    backgroundColor: '#FFE632',
+    borderWidth: 5,
+    borderColor: '#FFFFFF',
+    borderRadius: 5,
+    paddingTop: 10,
+    paddingBottom: 20
+  },
+  modalTitle: {
+    fontSize: 60,
+    color: 'white',
+    fontWeight: 'bold',
+    marginVertical: 10
+  },
+  modalText: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+    marginVertical: 10
+  }
 });
